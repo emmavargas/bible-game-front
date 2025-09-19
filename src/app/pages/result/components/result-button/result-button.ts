4a801f582d3d487544e0002cac90cbb38fa9402b
emma <emma.vargasvasquez@gmail.com>
2025-09-19 06:40:10 +0000

    version v11

--- src/app/pages/result/components/result-button/result-button.ts
+++ src/app/pages/result/components/result-button/result-button.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-result-button',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './result-button.html',
   styleUrl: './result-button.css'
 })
